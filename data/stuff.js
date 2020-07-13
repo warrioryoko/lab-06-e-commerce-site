514e0cfe5ab04d3e14aa3e941300a3688c0e7d07
warrioryoko <miangpellegri@gmail.com>
2020-07-13 15:24:03 -0700

    Created data for stuff

---
+++ data/stuff.js
@@ -0,0 +1,64 @@
+const oneFish = {
+  id: 'oneFish',
+  name: 'One Fish',
+  image: '../assets/one-fish.jpg',
+  description: 'Comes along with its pair',
+  category: 'fish',
+  price: 1.00
+};
+
+const twoFish = {
+  id: 'twoFish',
+  name: 'Two Fish',
+  image: '../assets/two-fish.jpg',
+  description: 'The second in its pair',
+  category: 'fish',
+  price: 2.00
+};
+
+const redFish = {
+  id: 'redFish',
+  name: 'Red Fish',
+  image: '../assets/red-fish.jpg',
+  description: 'The Red Fish',
+  category: 'fish',
+  price: 4.00
+};
+
+const blueFish = {
+  id: 'blueFish',
+  name: 'Blue Fish',
+  image: '../assets/blue-fish.jpg',
+  description: 'The Blue Fish',
+  category: 'fish',
+  price: 8.00
+};
+
+const thingOne = {
+  id: 'thingOne',
+  name: 'Thing One',
+  image: '../assets/thing-one.png',
+  description: "A thing that pays a visit to children learning to count",
+  category: 'thing',
+  price: 0.00
+}
+
+const thingTwo = {
+  id: 'thingTwo',
+  name: 'Thing Two',
+  image: '../assets/thing-two.jpg',
+  description: "A second thing that pays a visit to children learning to count",
+  category: 'thing',
+  price: 0.00
+}
+
+const stuff = [
+  oneFish,
+  twoFish,
+  redFish,
+  blueFish,
+  thingOne,
+  thingTwo
+];
+
+export default stuff;
