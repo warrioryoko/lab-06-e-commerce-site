aedccabcb7ea406cba8d4c648135029531b94007
warrioryoko <miangpellegri@gmail.com>
2020-07-19 14:18:29 -0700

    Hopefully fixed indentation issues

--- products/products.js
+++ products/products.js
@@ -6,5 +6,5 @@ const stuffList = document.getElementById('stuff');
 for (let i = 0; i < stuff.length; i++) {
   const myStuff = stuff[i];
   const element = renderStuff(myStuff);
-  stuffList.append(element);
+stuffList.append(element);
 }
