3de477fdcecbb1b896d420845230371455e0d794
warrioryoko <miangpellegri@gmail.com>
2020-07-22 10:38:11 -0700

    Got local storage working. Passes all basic requirements of lab; may add stretch goals later.

--- cart/shopping-cart.js
+++ cart/shopping-cart.js
@@ -1,12 +1,16 @@
-import cart from '../data/cart.js';
 import stuff from '../data/stuff.js';
 import { findById, calcOrderTotal } from '../common/utils.js';
 import renderTableRow from '../cart/render-line-item.js';
 
-console.log(cart);
-console.log(stuff);
 const tbody = document.querySelector('tbody');
 const orderTotalCell = document.getElementById('order-total-cell');
+const orderButton = document.getElementById('order-button');
+
+const makeEmptyCart = '[]';
+const actualCartInStorage = localStorage.getItem('CART') || makeEmptyCart;
+//Changed cart from a static data file, to the localstorage cart we pull or create here
+//Keeping naming consistent, this allows the loop to render items like before
+const cart = JSON.parse(actualCartInStorage);
 
 for (let i = 0; i < cart.length; i++) {
     const cartItem = cart[i];
@@ -20,3 +24,17 @@ for (let i = 0; i < cart.length; i++) {
 
 const orderTotal = calcOrderTotal(cart, stuff);
 orderTotalCell.textContent = orderTotal;
+
+//Place order button
+if (cart.length === 0) {
+    orderButton.disabled = true;
+} else {
+    orderButton.addEventListener('click', () => {
+        localStorage.removeItem('CART');
+
+        alert('You got STUFF:\n' + JSON.stringify(cart, true, 2));
+        // Play around with this...
+        window.location = '../';
+    });
+
+}
--- products/render-stuff.js
+++ products/render-stuff.js
@@ -1,3 +1,5 @@
+import { findById } from '../common/utils.js';
+
 function renderStuff(stuff) {
     const liElement = document.createElement('li');
     liElement.className = stuff.category;
@@ -18,7 +20,42 @@ function renderStuff(stuff) {
     const stuffButton = document.createElement('button');
     stuffButton.textContent = 'Add';
     stuffButton.value = stuff.id;
-    stuffButton.textContent = 'Add';
+    stuffButton.addEventListener('click', () => {
+        const makeEmptyCart = '[]';
+        const actualCartInStorage = localStorage.getItem('CART') || makeEmptyCart;
+        const cart = JSON.parse(actualCartInStorage);
+    
+        let stuffInCart = findById(cart, stuff.id);
+
+        //If there isn't a match IN THE CART of the item clicked, add one
+        if (!stuffInCart) {
+            const cartItem = {
+                id: stuff.id,
+                quantity: 1
+            };
+
+            //cart is an array, until it's serialized a string
+            //Put the cartItem into this array
+            cart.push(cartItem);
+        } else {
+            //The cart exists, and we've matched the item that was clicked
+            //Add one to the number of that item in the cart
+            stuffInCart.quantity++;
+        }
+
+        //Make the cart array into a string
+        const stringStuffCart = JSON.stringify(cart);
+        //setItem blows away everything in localStorage matching the name 'Cart' with what we pass to it
+        localStorage.setItem('CART', stringStuffCart);
+
+        alert('Hey, you added 1 ' + stuff.name + ' to your cart.');
+        //Sanity check: 
+        //1.) We correctly identified what we just clicked
+        //2.) We have a cart with all of the items that we should
+        //3/) We actually threw the cart into localStorage, where the cart now lives
+        console.log(stuffInCart, stringStuffCart, localStorage.getItem('CART'));
+    });
+
     pElement.append(stuffButton);
 
     liElement.append(pElement);
